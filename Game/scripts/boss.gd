9207e52a1ece9fcf844813a0ce271c2d1793d15e
Muhammad Zaman <98908904+MTZamanpk@users.noreply.github.com>
2024-04-08 02:14:20 -0600

    edited names

--- Game/scripts/boss.gd
+++ Game/scripts/boss.gd
@@ -54,7 +54,7 @@ func _on_hitbox_body_exited(body):
 func takingDamage():
 	if playerInAttackRange and global.playerCurrentAttack == true:
 		if canTakeDamage == true:
-			health = health - 20
+			health = health - 40
 			$AnimatedSprite2D.play("sideAttack")
 			$recieveDamageCooldown.start()
 			canTakeDamage = false
